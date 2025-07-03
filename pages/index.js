d800b648cb27708dc083c0d2b0969039b8f5dfda
teshaan23 <sanjbfx@gmail.com>
2025-07-03 02:04:07 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,3 +0,0 @@
-import dynamic from 'next/dynamic';
-const Dashboard = dynamic(() => import('../components/TradingJournalDashboard'), { ssr: false });
-export default function Home() { return <Dashboard />; }
